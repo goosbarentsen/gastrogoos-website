a06037e2475f5a28d3afcf9f6fef5623c2df27da
goosbarentsen <63584573+goosbarentsen@users.noreply.github.com>
2020-04-13 18:16:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from personal_portfolioe.wsgi import application
+from personal_portfolio.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
